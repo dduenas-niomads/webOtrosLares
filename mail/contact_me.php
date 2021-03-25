2f04dfc79d799001efb721a42f69d487cd5e9696
Alvaro Acosta <amacostas92@gmail.com>
2021-03-25 09:11:43 -0500

    formulario de contacto

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,24 +1,25 @@
 <?php
-// Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-	echo "No arguments Provided!";
-	return false;
-   }
-$name = $_POST['name'];
-$email_address = $_POST['email'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
-// Create the email and send the message
-$to = 'forbetterweb@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";
-mail($to,$email_subject,$email_body,$headers);
-return true;
+
+    $to = "contacto@otroslaresperu.com";
+    $subject = "Contacto: Formulario de Contacto Web Otros Lares";
+    $message = '<html><body>';
+    $message = $message . '<table rules="all" style="border-color: #666;" cellpadding="10">';
+    $message = $message . "<tr style='background: #eee;'><td><strong>Nombres completos: </strong> </td><td>" . strip_tags($_POST['name']) . "</td></tr>";
+    $message = $message . "<tr><td><strong>Email: </strong> </td><td>" . strip_tags($_POST['email']) . "</td></tr>";
+    $message = $message . "<tr style='background: #eee;'><td><strong>Mensaje: </strong> </td><td>" . strip_tags($_POST['message']) . "</td></tr>";
+    $message = $message . "</table>";
+    $message = $message . "</body></html>";
+    // It is mandatory to set the content-type when sending HTML email
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+    // More headers. From is required, rest other headers are optional
+    $headers .= 'From: contacto@otroslaresperu.com' . "\r\n";
+
+    $mail = mail($to, $subject, $message, $headers);
+    if ($mail){
+        echo "Mensaje enviado";
+    } else {
+        echo "No se pudo enviar el mensaje";
+    }
 ?>
